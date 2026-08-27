799e846b745019d9450fd91cd32a5ed6b4253e18
bhaumikmango <superbhaumik@gmail.com>
2026-08-27 18:56:53 +0530

    Django Shell and Migrations

--- core/home/models.py
+++ core/home/models.py
@@ -7,9 +7,13 @@ class Student(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     email = models.EmailField()
-    address = models.TextField()
+    address = models.TextField(null = True, blank = True)
     image = models.ImageField()
     file = models.FileField()
 
-class Product(models.Model):
-    pass
+class Car(models.Model):
+    name = models.CharField(max_length=150)
+    speed = models.IntegerField(default=64)
+
+    def __str__(self):
+        return self.name
---
+++ core/home/utils.py
@@ -0,0 +1,5 @@
+from home.models import Student
+
+def run_func():
+    print("Function Started")
+    print("Function Ended")
